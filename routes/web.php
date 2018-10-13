ff39a78f91dc9ffbaad92d4006f2251125e3c35b
Doris Morales <dmorales55@gmail.com>
2018-10-13 17:45:30 -0500

    09-Login Parte I

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,32 @@
 use App\Post;
 
 Route::get('/', function () {
-  $posts = Post::latest('published_at')->get();
+	$posts = Post::latest('published_at')->get();
 
-  return view('welcome', compact('posts'));
+	return view('welcome', compact('posts'));
 });
 
 Route::get('posts', function () {
-  return Post::all();
+	return Post::all();
 });
 
 Route::get('admin', function () {
 	return view('admin.dashboard');
 });
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+// if ($options['register'] ?? true) {
+// 	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// 	Route::post('register', 'Auth\RegisterController@register');
+// }
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
