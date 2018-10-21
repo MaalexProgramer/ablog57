f525cd29851d0f3aa9a314aeb531f2952022fd4f
Doris Morales <dmorales55@gmail.com>
2018-10-21 18:01:42 -0500

    60-Activar rutas anidadas

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -20,6 +20,7 @@ class UsersController extends Controller
   
   public function create()
   {
+    return view('admin.users.create');
   }
   
   public function store(Request $request)
@@ -33,9 +34,9 @@ class UsersController extends Controller
   
   public function edit(User $user)
   {
-    $roles = Role::pluck('name', 'id');
+    $roles = Role::with('permissions')->get();
     $permissions = Permission::pluck('name', 'id');
-    
+
     return view('admin.users.edit', compact('user', 'roles', 'permissions'));
   }
   
