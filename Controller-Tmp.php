9c31f43d93654ada8c236970ae5ad93979cb7e57
Doris Morales <dmorales55@gmail.com>
2018-10-21 12:37:22 -0500

    54-Mostrar los usuarios

---
+++ Controller-Tmp.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+  public function index()
+  {
+  }
+  
+  public function create()
+  {
+  }
+  
+  public function store(Request $request)
+  {
+  }
+  
+  public function show(User $user)
+  {
+  }
+  
+  public function edit(User $user)
+  {
+  }
+  
+  public function update(Request $request, User $user)
+  {
+  }
+  
+  public function destroy(User $user)
+  {
+  }
+}
---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+  public function index()
+  {
+    $users = User::get();
+
+    return view('admin.users.index', compact('users'));
+  }
+  
+  public function create()
+  {
+  }
+  
+  public function store(Request $request)
+  {
+  }
+  
+  public function show(User $user)
+  {
+  }
+  
+  public function edit(User $user)
+  {
+  }
+  
+  public function update(Request $request, User $user)
+  {
+  }
+  
+  public function destroy(User $user)
+  {
+  }
+}
