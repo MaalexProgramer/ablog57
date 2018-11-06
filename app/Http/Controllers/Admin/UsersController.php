5c0f9b1f61a496e7cede83e4174ee42c06f81f11
Doris Morales <dmorales55@gmail.com>
2018-11-06 11:58:41 -0500

    65-Politicas de acceso a usuarios

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -14,23 +14,28 @@ class UsersController extends Controller
 {
   public function index()
   {
-    $users = User::get();
+		$users = User::get();
 
     return view('admin.users.index', compact('users'));
   }
-  
+
   public function create()
   {
-    $user = new User;
+		$user = new User;
+		
+		$this->authorize('create', $user);
+
     $roles = Role::with('permissions')->get();
     $permissions = Permission::pluck('name', 'id');
 
     return view('admin.users.create', compact('user', 'roles', 'permissions'));
 
   }
-  
+
   public function store(Request $request)
   {
+		$this->authorize('create', new User);
+
     $data = $request->validate([
       'name' => 'required|max:255',
       'email' => 'required|email|max:255|unique:users',
@@ -60,20 +65,24 @@ class UsersController extends Controller
     // Regresamos al usuario
     return redirect()->route('admin.users.index')->with('success', 'El usuario ha sido creado');
   }
-  
+
   public function show(User $user)
   {
+		$this->authorize('view', $user);
+
     return view('admin.users.show', compact('user'));
   }
-  
+
   public function edit(User $user)
   {
+		$this->authorize('update', $user);
+
     $roles = Role::with('permissions')->get();
     $permissions = Permission::pluck('name', 'id');
 
     return view('admin.users.edit', compact('user', 'roles', 'permissions'));
   }
-  
+
   public function update(UpdateUserRequest $request, User $user)
   {
 /*     $data = $request->validate([
@@ -88,13 +97,15 @@ class UsersController extends Controller
     if($request->filled('password'))
     {
       $rules['password'] = ['confirmed', 'min:6'];
-    } */
+		} */
+
+		$this->authorize('update', $user);
 
     $user->update( $request->validated() );
 
     return back()->with('success', 'Usuario actualizado');
   }
-  
+
   public function destroy(User $user)
   {
   }
